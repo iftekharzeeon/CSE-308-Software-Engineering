b6953cc596cc726b93e064755aade7bebc8e042f
iftekharzeeon <1805038@ugrad.cse.buet.ac.bd>
2022-06-17 22:47:04 +0600

    Offline 3 Input file relocated

--- Offline 3 Structural Design Pattern/src/problem_1/Main.java
+++ Offline 3 Structural Design Pattern/src/problem_1/Main.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        File inputFile = new File("input.txt");
+        File inputFile = new File("src/problem_1/input.txt");
         DecimalFileAdder fileAdder = new DecimalFileAdder();
 
         fileAdder.calculateSum(inputFile);
